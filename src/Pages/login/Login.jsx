e73874f87499427074d7dd0eed8fcc91560bedb2
Abhijeet2010 <abhijeetraut220@gmail.com>
2024-08-03 19:19:22 +0530

    added register, login functioanlity from backend and works properly, logout remaining

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -1,7 +1,79 @@
+import React, { useState } from "react";
 import "./login.scss";
+import apiRequest from "../../lib/apiRequest.js";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    setLoading(true);
+
+    const formData = new FormData(e.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const response = await apiRequest.post("/auth/login", {
+        email,
+        password,
+      });
+      // Handle successful login
+      setLoading(false);
+      if (response.status === 200) {
+        localStorage.setItem("user", JSON.stringify(response.data.data));
+        navigate("/");
+      }
+    } catch (error) {
+      setError(
+        error.response ? error.response.data.message : "An error occurred"
+      );
+      setLoading(false);
+      console.error(
+        error.response ? error.response.data.message : error.message
+      );
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="loginContainer">
+        <div className="wrapper">
+          <h1>Login Here</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              name="email"
+              id="email"
+              type="email"
+              placeholder="Enter Your Email"
+              required
+            />
+            <input
+              name="password"
+              id="password"
+              type="password"
+              placeholder="Enter Your Password"
+              required
+            />
+
+            <button type="submit" disabled={loading}>
+              {loading ? "Logging in..." : "Login"}
+            </button>
+
+            {error && <p className="error">{error}</p>}
+          </form>
+        </div>
+      </div>
+
+      <div className="imgContent">
+        <img src="/bg.png" alt="Background" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Pages/register/Register.jsx
+++ src/Pages/register/Register.jsx
@@ -1,7 +1,9 @@
 import "./register.scss";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const Register = (e) => {
+const Register = () => {
+  const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
@@ -11,48 +13,50 @@ const Register = (e) => {
 
     try {
       const response = await axios.post(
-        "http://localhost:3000/api/auth/register",
+        "/auth/register",
         {
           username,
           email,
           password,
         }
       );
-
-      const newData = await response.data;
-
-      console.log(newData);
+      console.log(response);
+      if (response.status === 200) navigate("/login");
     } catch (error) {
-      console.log(error.message);
+      console.log(error.response.data.message);
     }
   };
+
   return (
     <div className="register">
       <div className="registerContainer">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Please Enter Username"
-          />
+        <div className="wrapper">
+          <h1>Create a new account</h1>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Please Enter Username"
+            />
 
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Please Enter Email"
-          />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Please Enter Email"
+            />
 
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Please Enter Password"
-          />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Please Enter Password"
+            />
 
-          <button>Submit</button>
-        </form>
+            <button>Submit</button>
+          </form>
+        </div>
       </div>
       <div className="imgContent">
         <img src="/bg.png" alt="" />
